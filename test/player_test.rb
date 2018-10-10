63dea37f14b2c79cd8c876d0f23f81e0f1e0acaa
Anten Skrabec <cebarks@gmail.com>
2018-10-10 15:37:30 -0600

    Add test/player_test.rb

---
+++ test/player_test.rb
@@ -0,0 +1,24 @@
+require './test/test_helper'
+require './lib/player'
+require './lib/ship'
+
+class PlayerTest < Minitest::Test
+  def setup
+    @human = HumanPlayer.new
+    @ai = AIPlayer.new
+  end
+
+  def test_has_ships
+    @human.add_ship("A1", "A3", 3) # creates Ship object in this method and pass it to the board and adds it to ships array
+    @ai.add_ship("A1", "A3", 3)
+    assert_equal 1, @human.ships.length
+    assert_equal 1, @ai.ships.length
+  end
+
+  def test_has_board
+    assert_instance_of Board, @human.board
+    assert_instance_of Board, @ai.board
+  end
+
+
+end
