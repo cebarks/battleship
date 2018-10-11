cb1602af951f26732e55722fa47c95b92419128b
Anna Smolentzov <annasmolentzov@gmail.com>
2018-10-10 20:25:18 -0600

    Add abilty for AI player to place ships

--- test/player_test.rb
+++ test/player_test.rb
@@ -1,33 +1,38 @@
 require './test/test_helper'
 require './lib/player'
 require './lib/ship'
+require './lib/ai_player'
+require './lib/board'
 
 class PlayerTest < Minitest::Test
   def setup
-    @human = HumanPlayer.new
+  #  @human = HumanPlayer.new
     @ai = AIPlayer.new
   end
 
   def test_has_ships
-    @human.add_ship("A1", "A3", 3) # creates Ship object in this method and pass it to the board and adds it to ships array
+    skip
+  #  @human.add_ship("A1", "A3", 3) # creates Ship object in this method and pass it to the board and adds it to ships array
     @ai.add_ship("A1", "A3", 3)
-    assert_equal 1, @human.ships.length
+    # This is basically a passthrough - consider removing and just passing straing to Board method add_ship
+  #  assert_equal 1, @human.ships.length
     assert_equal 1, @ai.ships.length
   end
 
   def test_has_board
-    assert_instance_of Board, @human.board
+#    assert_instance_of Board, @human.board
     assert_instance_of Board, @ai.board
   end
 
   def test_ai_can_place_ships
     @ai.place_ships
     refute_equal [], @ai.ships
+    assert_instance_of Ship, @ai.ships[0]
   end
 
   def test_player_can_place_ships
     skip
-    @human.place_ships
+#    @human.place_ships
     refute_equal [], @human.ships
   end
 end
